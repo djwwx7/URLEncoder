04afa1726fd6ff898fd6943424326569e35e2cc6
djwwx7 <djwwx7@mail.missouri.edu>
2018-07-06 17:45:44 -0500

    Final Solution

---
+++ URLEncoder/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLEncoder
+{
+    class Program
+    {
+        static Dictionary<string, string> conversions = new Dictionary<string, string>
+        {
+            {" ", "%20"},
+            {"<", "%3C"},
+            {">", "%3E"},
+            {"#", "%23"},
+            {"\"", "%22"},
+            {";", "%3B"},
+            {"/", "%2F"},
+            {"?", "%3F"},
+            {":", "%3A"},
+            {"@", "%40"},
+            {"&", "%26"},
+            {"=", "%3D"},
+            {"+", "%2B"},
+            {"$", "%24"},
+            {",", "%2C"},
+            {"{", "%7B"},
+            {"}", "%7D"},
+            {"|", "%7C"},
+            {"\\", "%5C"},
+            {"^", "%5E"},
+            {"[", "%5B"},
+            {"]", "%5D"},
+            {"`", "%60"}
+        };
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Please enter the name of the project:");
+                string project_name = encode(validInput());
+
+                Console.Write("Please enter the name of the activity: ");
+                string activity_name = encode(validInput());
+
+                Console.WriteLine("https://companyserver.com/content/{0}/files/{1}/{1}Report.pdf", project_name, activity_name);
+
+                Console.Write("Would you like to encode another url? (yes/no): ");
+            }
+            while (Console.ReadLine().ToLower().Equals("yes"));
+        }
+
+        static string validInput()
+        {
+            bool valid = false;
+            string input = null;
+
+            while (!valid)
+            {
+                input = Console.ReadLine();
+                valid = true;
+
+                foreach (char c in input.ToCharArray())
+                {
+                    if (Char.IsControl(c) || c == 0x7F)
+                    {
+                        Console.WriteLine("Input contained an invalid character, please re-enter: ");
+                        valid = false;
+                    }
+                }
+            }
+
+            return input;
+        }
+
+        static string encode(string value)
+        {
+            value = value.Replace("%", "%25");
+
+            foreach (var conversion in conversions)
+            {
+                value = value.Replace(conversion.Key, conversion.Value);
+            }
+
+            return value;
+        }
+    }
+}
---
+++ URLEncoder/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("URLEncoder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("URLEncoder")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("85962275-6dcc-4011-b085-cd306c9b9732")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
